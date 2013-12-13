6d0c1708544ac8c8e04a18c61db6f4a15d2ac6ce
tonyp <tonyptonyp@gmail.com>
2013-12-13 17:50:04 +0100

    add launch script

---
+++ vendorsetup.sh
@@ -0,0 +1,77 @@
+#
+# Copyright 2012 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+add_lunch_combo cm_p990-userdebug
+
+echo ""
+echo "Applying patches for P990"
+echo ""
+
+echo "Apply patch to frameworks/base"
+echo -n "Apply patch 0001-framework-base-patch.patch"
+(cd frameworks/base; git am ../../device/lge/p990/patches/0001-framework-base-patch.patch) > /dev/null 2>&1
+if [ $? == 0 ]; then
+	echo "     [DONE]"
+else
+	(cd frameworks/base; git am --abort)
+	echo "     [FAIL]"
+fi
+
+echo "Apply patch to frameworks/native"
+echo -n "Apply patch 0001-framework-native-fix.patch"
+(cd frameworks/native; git am ../../device/lge/p990/patches/0001-framework-native-fix.patch) > /dev/null 2>&1
+if [ $? == 0 ]; then
+	echo "     [DONE]"
+else
+	(cd frameworks/native; git am --abort)
+	echo "     [FAIL]"
+fi
+echo -n "Apply patch 0002-DisplayDevice-Backwards-compatibility-with-old-EGL.patch"
+(cd frameworks/native; git am ../../device/lge/p990/patches/0002-DisplayDevice-Backwards-compatibility-with-old-EGL.patch) > /dev/null 2>&1
+if [ $? == 0 ]; then
+	echo "     [DONE]"
+else
+	(cd frameworks/native; git am --abort)
+	echo "     [FAIL]"
+fi
+echo -n "Apply patch 0003-libgui-Bring-back-support-for-mHeap-based-screenshot.patch"
+(cd frameworks/native; git am ../../device/lge/p990/patches/0003-libgui-Bring-back-support-for-mHeap-based-screenshot.patch) > /dev/null 2>&1
+if [ $? == 0 ]; then
+	echo "     [DONE]"
+else
+	(cd frameworks/native; git am --abort)
+	echo "     [FAIL]"
+fi
+
+echo "Apply patch to frameworks/av"
+echo -n "Apply patch 0001-ifdef-for-ICS-Audio-Blob-compatibility.patch"
+(cd frameworks/av; git am ../../device/lge/p990/patches/0001-ifdef-for-ICS-Audio-Blob-compatibility.patch) > /dev/null 2>&1
+if [ $? == 0 ]; then
+	echo "     [DONE]"
+else
+	(cd frameworks/av; git am --abort)
+	echo "     [FAIL]"
+fi
+
+echo -n "Apply patch 0002-Add-missing-functions-and-signatures-for-older-OMX-v.patch"
+(cd frameworks/av; git am ../../device/lge/p990/patches/0002-Add-missing-functions-and-signatures-for-older-OMX-v.patch) > /dev/null 2>&1
+if [ $? == 0 ]; then
+	echo "     [DONE]"
+else
+	(cd frameworks/av; git am --abort)
+	echo "     [FAIL]"
+fi
+
