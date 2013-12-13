2c6307ecc564838a23591dc99c59ed5e80d76ecc
tonyp <tonyptonyp@gmail.com>
2013-12-13 21:14:06 +0100

    oups forgot the directory

--- patches/temp_reverts.sh
+++ patches/temp_reverts.sh
@@ -1,2 +1,5 @@
+echo -n "revert conflicting commits"
+cd frameworks/av
 git revert e407fffb9727fa283504b045635f1be34b759612 --no-edit
 git revert beb60baa3d79969b7513fd891f4ad86102512bbf --no-edit
+cd ../..
